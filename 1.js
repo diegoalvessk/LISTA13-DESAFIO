38bc1209734bfac8e6c93b6e7e66a63ad3ca98da
Diego Alves <diegoteialves@gmail.com>
2022-10-25 20:18:03 -0300

    Exercicio 1 Faça uma função que recebe um valor inteiro e verifica se o

---
+++ 1.js
@@ -0,0 +1,21 @@
+/* 
+1. Faça uma função que recebe um valor inteiro e verifica se o
+valor é positivo, negativo ou zero. A função deve retornar 1
+para valores positivos, -1 para valores negativos e 0 para o
+valor 0.
+*/
+var numero = verificarNumero(numero)
+
+function verificarNumero(numero){
+    numero = parseInt(prompt("Infomre um número: "))
+
+    if(numero == 0){
+        alert("O número é 0.")
+    }
+    if(numero>0){
+        alert("O número é positivo.")
+    }
+    if(numero<0){
+        alert("O número é negativo.")
+    }
+}
