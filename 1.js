9046648ef161d75057b4e95e869486a11f0861f7
Diego Alves <diegoteialves@gmail.com>
2022-11-02 22:18:57 -0300

    Verificar numero

--- 1.js
+++ 1.js
@@ -1,9 +1,3 @@
-/* 
-1. Faça uma função que recebe um valor inteiro e verifica se o
-valor é positivo, negativo ou zero. A função deve retornar 1
-para valores positivos, -1 para valores negativos e 0 para o
-valor 0.
-*/
 var numero = verificarNumero(numero)
 
 function verificarNumero(numero){
